78bab1efdb1d2231b665b5e52217337e78a315cf
Queenen <AnnBjo66962@stud.noroff.no>
2023-05-18 16:10:48 +0200

    index fetch data

--- js/index.js
+++ js/index.js
@@ -93,3 +93,109 @@ function slide3() {
   secondSlide.style.display = "none";
   thirdSlide.style.display = "flex";
 }
+
+// Slider Content
+
+const postContainer = document.querySelector(".post_container");
+const eachSlide = document.querySelector(".r_slide");
+
+const baseUrl = "https://www.rainydaysshop.no/wp-json/wp/v2";
+const postsUrl = "/posts";
+const mediaUrl = "/media";
+const categoryUrl = "/categories";
+const tagsUrl = "/tags";
+
+function fetchContent() {
+  async function fetchImages() {
+    const response = await fetch(baseUrl + mediaUrl);
+    const results = await response.json();
+    //console.log(results);
+    results.forEach((images) => {
+      const imgSrc = images.guid.rendered;
+      const altText = images.alt_text;
+    });
+  }
+  fetchImages();
+
+  async function fetchInfo() {
+    const response = await fetch(baseUrl + postsUrl);
+    const results = await response.json();
+    //console.log(results);
+    results.forEach((info) => {
+      const id = info.id;
+      const date = info.date.substring(0, 10);
+      const title = info.title.rendered;
+      const description = info.excerpt.rendered;
+      //console.log(info.tags);
+      info.tags.forEach((tags) => {});
+      info.categories.forEach((categories) => {});
+    });
+  }
+  fetchInfo();
+}
+fetchContent();
+
+/*async function fetchImages() {
+    try {
+      const response = await fetch(baseUrl + mediaUrl);
+      const results = await response.json();
+      //console.log(results);
+
+      for (let i = 0; i < results.length; i++) {
+        const imageSrc = results[i].guid.rendered;
+        postContainer.innerHTML = `<center><div class="r_slide post-1">
+        <img src="${imageSrc}"/></div></center>`;
+        //console.log(imageSrc);
+      }
+    } catch (error) {
+      console.log(error);
+      postContainer.innerHTML = error;
+    }
+  }
+
+  async function fetchContent() {
+    try {
+      const response = await fetch(baseUrl + postsUrl);
+      const results = await response.json();
+      //console.log(results);
+
+      // TAGS
+      for (let i = 0; i < results.length; i++) {
+        let tags = results[i].tags;
+        //console.log(tags);
+        for (let i = 0; i < tags.length; i++) {
+          tags = tags[i];
+          //console.log(tags);
+        }
+      }
+
+      // CATEGORIES
+      for (let i = 0; i < results.length; i++) {
+        let categories = results[i].categories;
+        //console.log(categories);
+        for (let i = 0; i < categories.length; i++) {
+          categories = categories[i];
+          //console.log(categories);
+        }
+      }
+
+      // TITLE, DATE & DESCRIPTION
+
+      for (let i = 0; i < results.length; i++) {
+        const title = results[i].title.rendered;
+        const date = results[i].date;
+        const length = 10;
+        const newDate = date.substring(0, length);
+        const description = results[i].excerpt.rendered;
+        console.log(description);
+      }
+    } catch (error) {
+      console.log(error);
+      postContainer.innerHTML = error;
+    }
+  }
+  fetchImages();
+  fetchContent();
+}
+
+fetchContent();*/
